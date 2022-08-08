a5c24335ab92043af4229a9f4c8d39b1dfd89e4f
Ross Wilson <ross.wilson@homeflow.co.uk>
2022-08-08 08:23:16 +0100

    Added dependcies and edited README

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+],
+  theme: {
+    extend: {},
+    colors: {
+      primary: '',
+      secondary: '',
+      warning: '',
+      success: '',
+      'primary-text': '',
+      background: ''
+    }
+  },
+  plugins: [],
+}
